d20478dbdaa83bc5f019be011b4b952e82b9306b
Niklas Reiprich <niklas@reiprich.com>
2024-05-28 16:07:13 +0200

    Test5

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You asgfghfghfadscgfhhgfghggggfafdsdfsadsa" />
+      <HelloWorld msg="You asgfghfghdefgzuiopüpoikujzgfdsafadscgfhhgfghggggfafdsdfsadsa" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
